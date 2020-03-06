edf45834300fe9de521439fd2ce8bbe600faf332
devProjetsAlex <alexandre.cote.5@gmail.com>
2020-03-06 16:39:09 -0500

    commit

--- src/component/Navigation/Navigation.jsx
+++ src/component/Navigation/Navigation.jsx
@@ -7,10 +7,10 @@ const Navigation =() => {
         <>
      
         <Button crokpot > Accueil </Button>
-        <Button crokpot>  Site SQDC </Button>
         <Button crokpot> Menu Disponbible </Button>
         <Button crokpot> Mixologie </Button>
         <Button crokpot> Prendre une <br/>réservation </Button>
+        <Button crokpot>  Site SQDC </Button>
         
         </>
     )
