d22921fdfb0dbaf244450096c932285373ef99dd
devProjetsAlex <alexandre.cote.5@gmail.com>
2020-03-06 21:22:48 -0500

    commit

---
+++ src/component/Navigation/Navigation.styles.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+import {Link} from 'react-router-dom'
+
+export const OptionLink = styled(Link)`
+  padding: 10px 15px;
+  cursor: pointer;
+`;
---
+++ src/container/Accueil/Accueil.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Accueil.css'
+
+const Accueil = () => (
+    <div className='Accueil'>Accuel</div>
+)
+
+export default Accueil
